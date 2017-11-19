1a9ecc5e28d4ce3e6b743d2f9afc1d896fa3df78
TBisen04 <tanuj.bisen@gmail.com>
2017-11-19 17:21:30 +0530

    çommit changes

--- src/GFG.java
+++ src/GFG.java
@@ -7,57 +7,72 @@ class GFG {
 	public static void main(String[] args) {
 		// code
 		try {
-			BufferedReader br = new 
-					BufferedReader(new InputStreamReader(System.in));
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			int T = Integer.parseInt(br.readLine());
 
 			for (int t = 1; t <= T; t++) {
 				int n = Integer.parseInt(br.readLine());
-				int a[] = new int [n];
-				int k=0;
-                int max = -100000;
-				
+				int a[] = new int[n];
+				int b[] = new int[n];
+				int c[] = new int[n];
+				String line = br.readLine();
+				int k = 0;
+				int l = 0;
+				for (String numStr : line.split("\\s")) {
+					int temp = Integer.parseInt(numStr);
 
-				String line = null;
-				line = br.readLine();
-              while (line !=null){
-            	  
-					for (String numStr : line.split("\\s")) {
-						int temp = Integer.parseInt(numStr);
-						if ( temp > max){
-							max=temp;
+					if (temp < 0) {
+						a[k++] = -temp;
+
+					} else {
+						b[l++] = temp;
+
+					}
+
+				}
+				k = 0;
+				l = 0;
+				Arrays.sort(a);
+				Arrays.sort(b);
+
+				k = 0;
+				for (int i = 0; i < n; i++) {
+					if (b[i] == 0) {
+						continue;
+					}
+					for (int j = 0; j < n; j++) {
+
+						if (a[j] == 0) {
+							continue;
 						}
-						a[k++]=temp;
-						
-					}
-					if ( k == n){
-						break;
-					}
-					line = br.readLine();
-            	  
-               }
-             
-				int[] result = new int[max];
-				for (int i=0; i < n ; i++) {
-					
 
-					result[a[i] - 1] = result[a[i] - 1] + 1;
+						if (b[i] == a[j]) {
+							c[k++] = -a[j];
+							c[k++] = b[i];
+							b[i] = 0;
+							a[j] = 0;
+							break;
+						}
+
+					}
 				}
 
-				int temp = 0;
-				
-				for (int i = 0; i < max; i++) {
-					if (result[i] != 0) {
-						temp = temp + result[i];
-						System.out.print(temp + " ");
-					}
+				boolean check = false;
+				for (int i = 0; i < n; i++) {
 
+					if (c[i] != 0) {
+						check = true;
+						System.out.print(c[i] + " ");
+					}
+				}
+				if (!check) {
+					System.out.print(0);
 				}
 				System.out.println();
 			}
 
 		} catch (Exception e) {
-          
+
 		}
 	}
 }
